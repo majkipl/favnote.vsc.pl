f9a0a5746787274b567799c058b0154111e76c86
majkipl <majkipl@gmail.com>
2020-10-09 15:18:17 +0200

    konfiguracja pod relatywny import

---
+++ src/views/Root/Root.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Button from "components/Button/Button";
+
+const Root = () => {
+  return (
+    <>
+      <h1>Hello Bolek</h1>
+      <Button>Helo helo</Button>
+    </>
+  );
+};
+
+export default Root;
+
+// TODO kontynuujemy jutro w auchowni :)
