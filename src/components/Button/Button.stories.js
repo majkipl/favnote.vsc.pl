09c2282d237b3ff30c1a4a9cecbd8e3044bbc674
majkipl <majkipl@gmail.com>
2020-10-11 01:17:01 +0200

    storybook - pierwszy komponent

---
+++ src/components/Button/Button.stories.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Button from "./Button";
+
+export default {
+  component: Button,
+  title: "Button",
+};
+
+export const Primary = () => <Button>Hello</Button>;
+export const Secondary = () => <Button secondary>Hello</Button>;
