4c0214d643fe27a4fbd902a0ea22926a02a2d074
majkipl <majkipl@gmail.com>
2020-10-14 22:13:47 +0200

    refaktoring aplikacji

--- src/templates/MainTemplate.js
+++ src/templates/MainTemplate.js
@@ -1,18 +1,73 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { ThemeProvider } from "styled-components";
 import GlobalStyle from "theme/GlobalStyle";
 import { theme } from "theme/mainTheme";
+import { withRouter } from "react-router";
 
-const MainTemplate = ({ children }) => (
-  <div>
-    <GlobalStyle />
-    <ThemeProvider theme={theme}>{children}</ThemeProvider>
-  </div>
-);
+class MainTemplate extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pageType: "notes",
+    };
+  }
 
+  componentDidMount() {
+    this.setCurrentPage();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    this.setCurrentPage(prevState);
+  }
+
+  setCurrentPage = (prevState = "") => {
+    const pageTypes = ["twitters", "articles", "notes"];
+    const {
+      location: { pathname },
+    } = this.props;
+
+    const [currentPage] = pageTypes.filter((page) => pathname.includes(page));
+
+    if (prevState.pageType !== currentPage) {
+      this.setState({ pageType: currentPage });
+      console.log(this.state);
+    }
+  };
+
+  render() {
+    const { children } = this.props;
+    return (
+      <div>
+        <GlobalStyle />
+        <ThemeProvider theme={theme}>{children}</ThemeProvider>
+      </div>
+    );
+  }
+}
+
+/*
+= ({ children, ...props }) => {
+  return (
+    <div>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    </div>
+  );
+};
+*/
 MainTemplate.propTypes = {
   children: PropTypes.element.isRequired,
+  location: PropTypes.shape({
+    pathname: PropTypes.string.isRequired,
+  }),
+  // location: PropTypes.oneOfType(PropTypes.func).isRequired,
 };
 
-export default MainTemplate;
+MainTemplate.defaultProps = {
+  location: {
+    pathname: "",
+  },
+};
+
+export default withRouter(MainTemplate); // <- Higher Order Component
